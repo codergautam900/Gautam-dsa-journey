85e02aa6294beffc38f204c74e7e1f810fb2bdee
Gautam sagar <gateaspirant8650@gmail.com>
2026-03-10 18:52:31 +0530

    Day-1 completed

---
+++ Day-1/First-program.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    // std::cout << "Hello Gautam" << std::endl;  // normal way print 
+
+      cout << "Hello Gautam" << endl;  // using namespace std;
+
+      int a = 10;      // variable declaration and initialization
+
+      cout << "The value of a is: " << a << endl;  // printing variable value
+
+      float b = 3.14;  // variable declaration and initialization
+
+      cout << "The value of b is: " << b << endl;  // printing variable value
+
+       long c = 1234567890;  // variable declaration and initialization
+
+        cout << "The value of c is: " << c << endl;  // printing variable value
+
+        double d = 3.1415926535;  // variable declaration and initialization
+
+        cout << "The value of d is: " << d << endl;  // printing variable value
+
+         char e = 'A';  // variable declaration and initialization
+
+         cout << "The value of e is: " << e << endl;  // printing variable value
+
+          bool f = true;  // variable declaration and initialization
+
+          cout << "The value of f is: " << f << endl;  // printing variable value
+
+
+
+
+
+
+    return 0;
+}
+
+
+
