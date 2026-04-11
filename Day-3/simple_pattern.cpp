e80f413f5c9c0fc86bcd6e7e436a99e1726bda8b
Gautam sagar <gateaspirant8650@gmail.com>
2026-04-11 19:26:49 +0530

    Day-3 completed

---
+++ Day-3/simple_pattern.cpp
@@ -0,0 +1,150 @@
+#include<iostream> 
+using namespace std;
+
+int main() {
+
+
+    // Question:-1 
+
+  /* cout<< "Enter the no: ";
+
+         int n;
+
+         cin >> n;
+
+         for(int row = 1; row<=n; row++){
+            for(int col = 1 ; col<=n; col++){
+
+                cout<<" * ";
+            }
+
+            cout << endl;
+
+
+         }
+
+         */
+
+
+            // Question-2 
+
+
+            // cout<< "Enter the no: ";
+            // int n;
+
+            // cin >> n;
+
+            // for(int row =1; row<=n; row++){
+            //      for(int col =1; col<=n; col++)
+            //      {
+            //         cout<< 10 << " " ;
+            //      }
+
+            //      cout<<endl;
+            // }
+
+
+
+            // Question:-3
+
+            // int n ;
+            // cout<<"Enter the row no: ";
+            // cin >> n;
+
+            // for(int row =1; row<=n; row++){
+            //     for(int col =1; col<=n; col++){
+
+            //         cout<<row<< " ";
+            //     }
+            //     cout<<endl;
+            // }
+
+
+
+            // Question:-4
+
+
+            // int n;
+            // cout<<"Enter the row no: ";
+            // cin >> n;
+            // for(int row =1; row<=n; row++){
+            //     for(int col =1; col<=n; col++){
+            //         cout<<col<<" ";
+            //     }    
+            //     cout<<endl;   
+            //      }
+
+
+            // Question:- 5 
+
+            // int n;
+            // cout<<"Enter the row no: ";
+            // cin >> n;
+            // for(int row = 1; row<=n; row++){
+            //     for(int col = n; col>=1; col--){
+            //         cout<<col << " ";
+
+            //     }
+
+            //     cout<< endl;
+            // }
+
+
+            // Question:-6
+
+
+            
+            // int n;
+            // cout<<"Enter the row no: ";
+            // cin >> n;
+            // for(int row =1; row<=n; row++){
+            //     for(int col =1; col<=n; col++){
+            //         cout<<col*col<<" ";
+            //     }    
+            //     cout<<endl;   
+            //      }
+
+
+              // Question:-7
+
+            int n;
+            cout<<"Enter the row no: ";
+            cin >> n;
+            for(int row =1; row<=n; row++){\
+
+                char name = 'a' + (row-1);
+
+                for(int col =1; col<=n; col++)
+
+                cout<< name << " ";
+
+                    
+                   
+                cout<<endl;   
+                 }
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          return 0;
+
+
+}
+
+
