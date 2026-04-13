956f9b1acdbf9134d802f1f9aa2419e0f5251a20
Gautam sagar <gateaspirant8650@gmail.com>
2026-04-13 23:58:32 +0530

    super hard pattern print

--- Day-5/super_hard_pattern.cpp
+++ Day-5/super_hard_pattern.cpp
@@ -32,6 +32,63 @@ int main() {
 
     // Question:- 18
 
+    // int n;
+    // cout<<"Enter the row: ";
+    // cin >> n;
+    // for(int row =1; row<=n; row++){
+    //     for(int col=1; col<=n-row; col++) {
+    //         cout<<" ";
+    //     }
+    //     for(int col=1; col<=row; col++){
+
+    //         cout<<"*" ;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+    // Question:- 19
+
+    // int n;
+    // cout<<"Enter the row: ";
+    // cin >> n;
+    // for(int row =1; row<=n; row++){
+    //     for(int col=1; col<=n-row; col++) {
+    //         cout<<" ";
+    //     }
+    //     for(int col=1; col<=row; col++){
+
+    //         cout<< row ;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    
+    // Question:- 20
+
+    // int n;
+    // cout<<"Enter the row: ";
+    // cin >> n;
+    // for(int row =1; row<=n; row++){
+    //     for(int col=1; col<=n-row; col++) {
+    //         cout<<" ";
+    //     }
+    //     for(int col=1; col<=row; col++){
+
+    //         cout<< col ;
+    //     } 
+    //     cout<<endl;
+    // }
+
+
+
+
+    // Question:- 21 
+     
+    
+
     int n;
     cout<<"Enter the row: ";
     cin >> n;
@@ -39,12 +96,20 @@ int main() {
         for(int col=1; col<=n-row; col++) {
             cout<<" ";
         }
-        for(int col=1; col<=row; col++){
+        for(char name = 'A'; name<= 'A' + row-1; name++){
 
-            cout<<"* " ;
-        }
+
+            cout<<name<< "";
+
+        } 
         cout<<endl;
     }
+
+
+
+
+
+
 
 
     
